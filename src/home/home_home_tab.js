ca6e7af988eb5ec51a2a3c11848ff042f09874dc
[Github <72160826+nrj022@users.noreply.github.com>
2021-08-27 02:32:03 +0900

    add product list on home tab

--- src/home/home_home_tab.js
+++ src/home/home_home_tab.js
@@ -8,7 +8,7 @@
 
 import 'react-native-gesture-handler';
 import React, {Component} from 'react';
-import {Text, View, Image, ImageBackground} from 'react-native';
+import {Text, View, Image, ImageBackground, ScrollView} from 'react-native';
 
 /* HomeEventContent = props => {
   return <ImageBackground source={require('../assets/pics/upcy_logo.png')} />;
@@ -19,50 +19,83 @@ const storeList1 = [
     name: 'MESSENGER MEDIUM',
     brand: 'FREITAG',
     price: '346,000',
-    image: require(''),
+    image: require('../src/assets/pics/freitag_messenger.jpg'),
   },
-  {name: 'POUCH EXTRA SMALL', brand: '', price: '44,000', image: require('')},
+  {
+    name: 'POUCH EXTRA SMALL',
+    brand: 'FREITAG',
+    price: '44,000',
+    image: require('../src/assets/pics/freitag_pouch.jpg'),
+  },
   {
     name: 'Back pleats hybrid T-shirt',
     brand: 'RE;CODE',
     price: '190,000',
-    image: require(''),
+    image: require('../src/assets/pics/re;code_tshirt.jpg'),
   },
   {
-    name: 'Recycled fabric shorts',
-    brand: '',
-    price: '99,000',
-    image: require(''),
+    name: 'REO893 토트백 ',
+    brand: '119REO',
+    price: '220,000',
+    image: require('../src/assets/pics/119reo_totbag.png'),
   },
 ];
 const storeList2 = [
   {
     name: 'airbag packable back-pack (S)',
-    brand: '',
-    price: '',
-    image: require(''),
+    brand: 'RE;CODE',
+    price: '149,000',
+    image: require('../src/assets/pics/re;code_bag.jpg'),
   },
   {
     name: 'LABEL_FRAME CLUTCH_HASSAM',
     brand: 'UL:KIN',
     price: '188,000',
-    image: require(''),
+    image: require('../src/assets/pics/ulkin_clutch.jpg'),
   },
   {
     name: '챌린저백 / challenger bag',
     brand: '119REO',
     price: '98,000',
-    image: require(''),
+    image: require('../src/assets/pics/119reo_bag.png'),
   },
-  {name: 'REO893 토트백 ', brand: '', price: '220,000', image: require('')},
+  {
+    name: 'Recycled fabric shorts',
+    brand: 'RE;CODE',
+    price: '99,000',
+    image: require('../src/assets/pics/re;code_shorts.jpg'),
+  },
 ];
+
+const storeColumn1 = storeList1.map(store => {
+  <View style={{marginBottom: 20}}>
+    <Image
+      style={{width: 200, height: 200, marginBottom: 20}}
+      source={store.image}
+    />
+    <Text style={{fontSize: 15, fontWeight: 700}}>{store.name}</Text>
+    <Text>{store.brand}</Text>
+    <Text>{store.price}</Text>
+  </View>;
+});
+const storeColumn2 = storeList2.map(store => {
+  <View style={{marginBottom: 20}}>
+    <Image
+      style={{width: 200, height: 200, marginBottom: 20}}
+      source={store.image}
+    />
+    <Text style={{fontSize: 15, fontWeight: 700}}>{store.name}</Text>
+    <Text>{store.brand}</Text>
+    <Text>{store.price}</Text>
+  </View>;
+});
 
 class HomeHomeScreen extends Component {
   render() {
     return (
       <View style={{margin: 15}}>
         <ImageBackground
-          source={require('../assets/pics/flower.jpg')}
+          source={require('../assets/pics/plastic_mill.png')}
           // imageBackground는 height, width 설정 필수
           style={{
             position: 'relative',
@@ -94,57 +127,12 @@ class HomeHomeScreen extends Component {
             {`이벤트 바로가기 >`}
           </Text>
         </ImageBackground>
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            margin: 15,
-          }}>
-          <View>
-            <Image
-              style={{width: 200, height: 200, marginBottom: 20}}
-              source={require('../assets/pics/flower-re.jpg')}
-            />
-            <Image
-              style={{width: 200, height: 200, marginBottom: 20}}
-              source={require('../assets/pics/flower-re.jpg')}
-            />
-            <Image
-              style={{width: 200, height: 200, marginBottom: 20}}
-              source={require('../assets/pics/flower-re.jpg')}
-            />
-            <Image
-              style={{width: 200, height: 200, marginBottom: 20}}
-              source={require('../assets/pics/flower-re.jpg')}
-            />
-            <Image
-              style={{width: 200, height: 200, marginBottom: 20}}
-              source={require('../assets/pics/flower-re.jpg')}
-            />
-          </View>
-          <View>
-            <Image
-              style={{width: 200, height: 200, marginBottom: 20}}
-              source={require('../assets/pics/flower-re.jpg')}
-            />
-            <Image
-              style={{width: 200, height: 200, marginBottom: 20}}
-              source={require('../assets/pics/flower-re.jpg')}
-            />
-            <Image
-              style={{width: 200, height: 200, marginBottom: 20}}
-              source={require('../assets/pics/flower-re.jpg')}
-            />
-            <Image
-              style={{width: 200, height: 200, marginBottom: 20}}
-              source={require('../assets/pics/flower-re.jpg')}
-            />
-            <Image
-              style={{width: 200, height: 200, marginBottom: 20}}
-              source={require('../assets/pics/flower-re.jpg')}
-            />
-          </View>
-        </View>
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          style={{marginTop: 20, paddingBottom: 150, paddingHorizontal: 20}}>
+          <storeColumn1 />
+          <storeColumn2 />
+        </ScrollView>
       </View>
     );
   }
